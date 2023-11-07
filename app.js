089c93d846b8bd4dcdc7c2d04b8fdeb977a4b421
unorji kingsley <unorjikingsley@gmail.com>
2023-11-07 22:36:00 +0100

    debug

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}..`)
     );
   } catch (error) {
     console.log(error);
